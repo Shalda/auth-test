6a8de399b6b45a97c0d1a69606131e39f67ca70c
Shalda <shalda.dmitry@gmail.com>
2021-10-28 16:43:40 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, ChangeDetectorRef, OnInit} from '@angular/core';
+import { Auth } from 'aws-amplify';
+import { onAuthUIStateChange, CognitoUserInterface, AuthState } from '@aws-amplify/ui-components';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'auth-app';
+export class AppComponent implements OnInit{
+  title = 'amplify-angular-auth';
+  user: CognitoUserInterface | undefined;
+  authState: AuthState;
+
+  constructor(private ref: ChangeDetectorRef) {}
+  signIn(providerName: string){
+    Auth.federatedSignIn({customProvider: providerName}).then(data=> console.log(data))
+  }
+
+  ngOnInit() {
+    onAuthUIStateChange((authState, authData) => {
+      this.authState = authState;
+      this.user = authData as CognitoUserInterface;
+      this.ref.detectChanges();
+      console.log(authState, authData)
+    })
+  }
+
+  ngOnDestroy() {
+    return onAuthUIStateChange;
+  }
 }
